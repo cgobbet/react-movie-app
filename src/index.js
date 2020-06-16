461f97e065e457371faf5e05f1a48487175711ff
cgobbet <cassiano.gobbet@gmail.com>
2020-06-16 16:50:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import SearchMovies from "./SearchMovies";
+// import SearchMovies from "./SearchMovies";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
