84b5f2190de44d1eac74755a3be9e9d372063798
cgobbet <cassiano.gobbet@gmail.com>
2020-06-16 17:55:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import YoutubeBackground from 'react-youtube-background'
 // import logo from './logo.svg';
 import './App.css';
 import SearchMovies from './SearchMovies';
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         {/* <img
@@ -12,10 +14,10 @@ function App() {
           className='App-logo'
           alt='zFlix Logo'>
           </img> */}
-        <h1>ZFlix</h1>
+        <h1>Search-O-Flix</h1>
       </header>
       <div className="container">
-        <h1 className="title">Search your movie</h1>
+        {/* <h1 className="title">Search your movie</h1> */}
         <SearchMovies />
       </div>
     </div>
